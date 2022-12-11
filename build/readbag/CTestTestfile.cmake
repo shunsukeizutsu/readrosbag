b872a643127a74240cd337381198c89e5e6e0247
desktopIzutsun <shunsuke.hk396@au.com>
2022-12-12 01:30:32 +0900

    create read_camera

--- build/readbag/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/shunsuke/readrosbag/src/readbag
-# Build directory: /home/shunsuke/readrosbag/build/readbag
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
---
+++ src/readbag/src/read_camera.cpp
@@ -0,0 +1,57 @@
+#include <ros/ros.h>
+#include <rosbag/bag.h>
+#include <rosbag/view.h>
+#include <vector>
+#include "std_msgs/String.h"
+#include <boost/foreach.hpp>
+#include <stdint.h>
+#include <stdio.h>
+#include <cstdint>
+#include "sensor_msgs/Image.h"
+
+int main()
+{
+    rosbag::Bag bag;
+    bag.open("/home/shunsuke/rosbag/2022-08-19-16-07-21.bag", rosbag::bagmode::Read);
+
+    std::string camera = "/usb_cam/image_raw";
+
+    std::vector<std::string> topics;
+    topics.push_back(camera);
+
+    rosbag::View view(bag, rosbag::TopicQuery(camera));
+
+    std::cout << "%time,field.header.seq,field.header.stamp,field.header.frame_id,";
+    std::cout << "field.height,field.width,field.encoding,field.is_bigendian,field.step,";
+    for(int i=0;i<921600;i++)
+    {
+        std::cout << "data" << i << ",";
+    }
+    std::cout << std::endl;
+
+    BOOST_FOREACH (rosbag::MessageInstance const m, view)
+    {
+        if (m.getTopic() == camera)
+        {
+            sensor_msgs::Image::ConstPtr msg = m.instantiate<sensor_msgs::Image>();
+            std::cout << "none" << ",";
+            std::cout << msg->header.seq << ",";
+            std::cout << msg->header.stamp << ",";
+            std::cout << msg->header.frame_id << ",";
+            std::cout << msg->height << ",";
+            std::cout << msg->width << ",";
+            std::cout << msg->encoding << ",";
+            std::cout << (int)msg->is_bigendian << ",";
+            std::cout << msg->step << ",";
+            int total_size=msg->step*msg->height;
+            //total_size=921600
+            for(int i=0;i<total_size;i++)
+            {
+                std::cout << (int)msg->data[i] << ",";
+            }
+            std::cout << std::endl;
+        }
+    }
+    bag.close();
+    return 0;
+}
